1d94418f8bc0fd3afcdb6a82bc72f472b90e3240
alfacharliekilo <acking@utexas.edu>
2023-05-20 23:21:40 -0500

    Stuff.

--- auth.py
+++ auth.py
@@ -3,10 +3,10 @@ import openai
 
 openai.api_key = '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'
 
-query = input("Provide a string to translate into French: "
+user_input = input("Provide a string to translate into French: "
 response = openai.Completion.create(
   engine="text-davinci-002",
-  prompt="Translate the following English text to French: '{}'",
+  prompt="Translate the following English text to French: '{user_input}'",
   max_tokens=60
 )
 
